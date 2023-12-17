1d4eabc8cf45c3a29110837548ad9cf6462e5b70
Pedro Cunha <idk.pedroc@gmail.com>
2023-12-17 12:34:49 -0300

    chore: house keeping

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 import os
 # from apscheduler.triggers.interval import IntervalTrigger
 # from mod.scheduler import create_scheduler
-import schedule
 
 USUARIOS_INTERESSADOS = {
 }
