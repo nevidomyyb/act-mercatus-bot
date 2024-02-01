ad02f744d9988b0ea8ec8807f71720781c38456c
Pedro Cunha <idk.pedroc@gmail.com>
2024-02-01 19:42:13 -0300

    multiprocessing is now working

--- app.py
+++ app.py
@@ -3,72 +3,84 @@ from telethon.sync import TelegramClient
 from dotenv import load_dotenv
 import os
 import multiprocessing
-from mod.scheduler import create_schedule
+from mod.scheduler import send_message_routine
+from time import sleep
+import asyncio
+from functools import partial
 # from apscheduler.triggers.interval import IntervalTrigger
 # from mod.scheduler import create_scheduler
 
-def create_string_by_monitored_actions(user_id):
+def create_string_by_monitored_actions(user_id, usuarios_interessados):
     return ", ".join(usuarios_interessados[user_id])
 
-async def check_and_add_to_user(user_id, client):
+async def check_and_add_to_user(user_id, client, usuarios_interessados):
     if user_id not in usuarios_interessados.keys():
         usuarios_interessados[user_id] = []
         await client.send_message(user_id, f"Você foi inserido na lista de usuários do Act Mercatus.")
         return False
+
+def run_client_scheduler(client_name, api_id, api_hash, bot_token, usuarios_interessados):
+    client = TelegramClient(client_name, api_id, api_hash)
+    client.start(bot_token=bot_token)
+    client.start()
+
+    print("Segundo cliente rodando")
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    for c in range(10):
+        print(f"Sending the {c}° messages.")
+        # await send_message_routine(client, usuarios_interessados)
+        partial_ = partial(send_message_routine, client, usuarios_interessados)
+        x = loop.run_until_complete(partial_())
+        sleep(10)
+        print("Messages sended.")
+    loop.close()
+    client.loop.stop()
     
 
-def run_client(client_dict, api_id, api_hash, bot_token, usuarios_interessados):
-    with TelegramClient('act_mercatus', api_id, api_hash) as client:
-        client.start(bot_token=bot_token)
-        
-        client_dict['session_path'] = 'act_mercatus'
-        print(client_dict)
-        
-        @client.on(events.NewMessage(pattern='/acao'))
-        async def nova_acao(event):
-            mensagem = event.raw_text
-            palavras = mensagem.split()
-            user_id = event.message.peer_id.user_id
-            rp = await check_and_add_to_user(user_id, client)
-            if len(palavras) == 2:
-                parametro = palavras[1]
-                usuarios_interessados[user_id].append(parametro)
-            else:
-                parametros = palavras[1:]
-                usuarios_interessados[user_id].extend(parametros)
-            acoes = create_string_by_monitored_actions(user_id)
-            await event.respond(f"As ações que vão ser monitoradas agora: {acoes}")
-
-        @client.on(events.NewMessage(pattern='/check'))
-        async def checar_acoes(event):   
-            user_id = event.message.peer_id.user_id
-            rp = await check_and_add_to_user(user_id, client)
-            acoes = create_string_by_monitored_actions(user_id)
-            await client.send_message(user_id, f"Ações monitoradas atualmente: {acoes}")
-            
-        client.run_until_disconnected()
+def run_client(client_name, api_id, api_hash, bot_token, usuarios_interessados):
+    client = TelegramClient(client_name, api_id, api_hash)
+    client.start(bot_token=bot_token)
     
-        
+    @client.on(events.NewMessage(pattern='/acao'))
+    async def nova_acao(event):
+        mensagem = event.raw_text
+        palavras = mensagem.split()
+        user_id = event.message.peer_id.user_id
+        rp = await check_and_add_to_user(user_id, client, usuarios_interessados)
+        if len(palavras) == 2:
+            parametro = palavras[1]
+            usuarios_interessados[user_id].append(parametro)
+        else:
+            parametros = palavras[1:]
+            usuarios_interessados[user_id].extend(parametros)
+        acoes = create_string_by_monitored_actions(user_id, usuarios_interessados)
+        await event.respond(f"As ações que vão ser monitoradas agora: {acoes}")
+
+    @client.on(events.NewMessage(pattern='/check'))
+    async def checar_acoes(event):   
+        user_id = event.message.peer_id.user_id
+        rp = await check_and_add_to_user(user_id, client, usuarios_interessados)
+        acoes = create_string_by_monitored_actions(user_id, usuarios_interessados)
+        await client.send_message(user_id, f"Ações monitoradas atualmente: {acoes}")
+    client.run_until_disconnected()
+
 if __name__ == "__main__":
-    usuarios_interessados = {
-    }
 
     load_dotenv()
     api_id = os.getenv("API_ID")
     api_hash = os.getenv("API_HASH")
     bot_token = os.getenv("BOT_TOKEN")
-
-    print('tafora')
+    print(api_id, api_hash, bot_token)
     with multiprocessing.Manager() as manager:
-        print('entro aq')
-        client_dict = manager.dict()
         
-        telegram_process = multiprocessing.Process(target=run_client, args=(client_dict, api_id, api_hash, bot_token, usuarios_interessados))
+        usuarios_interessados = {}
+        
+        telegram_process = multiprocessing.Process(target=run_client, args=('act_mercatus', api_id, api_hash, bot_token, usuarios_interessados))
         telegram_process.start()
-        
-        loop_process = multiprocessing.Process(target=create_schedule, args=(client_dict, api_id, api_hash, usuarios_interessados))
-        loop_process.start()
+        telegram_process2 = multiprocessing.Process(target=run_client_scheduler, args=('act_scheduler', api_id, api_hash, bot_token, usuarios_interessados))
+        telegram_process2.start()
         
         telegram_process.join()
-        loop_process.join()
+        telegram_process2.join()
         
--- mod/scheduler.py
+++ mod/scheduler.py
@@ -9,39 +9,35 @@ def create_string(user_data):
         str += f"{act}: R$ {user_data[act]}\n"
     return str
 
-def create_schedule(client_dict, api_id, api_hash, usuarios_interessados):
-    print("Teste")
-    sleep(10)
-    session_path = client_dict.get('session_path')
-    print(session_path)
-    if session_path:
-        with TelegramClient(session_path,api_id, api_hash) as client:
-            while client.client.loop.is_running():
-                send_message_routine(client, usuarios_interessados)
-                print('Messages sended')
-                sleep(2)
+
         
 async def send_message_routine(client, usuarios_interessados):
     prices = {}
-    for usuario in usuarios_interessados:
-        for act in usuarios_interessados[usuario]:
-            if act in prices.keys():
-                continue
-            all_data = check_act(act)
-            if all_data is False: 
-                await client.send_message(usuario, f"A ação '{act}' não é um papel válido.\n Removendo-a.")
-                continue
-            try:
-                prices[act] = all_data['currentPrice']
-            except:
-                prices[act] = all_data['previousClose']
-    user_with_prices = {}
-    for usuario in usuarios_interessados:
-        user_with_prices[usuario] = {}
-        for act in prices:
-            if act in usuarios_interessados[usuario]:
-                user_with_prices[usuario][act] = prices[act]
+    print('Entrou aqui pelo menos')
+    print(usuarios_interessados)
+    # for usuario in usuarios_interessados:
+    #     print('Agora entrou aqui rsrsrrs')
+    #     for act in usuarios_interessados[usuario]:
+    #         if act in prices.keys():
+    #             continue
+    #         all_data = check_act(act)
+    #         if all_data is False: 
+    #             print("It was not a valid paper")
+    #             await client.send_message(usuario, f"A ação '{act}' não é um papel válido.\n Removendo-a.")
+    #             continue
+    #         try:
+    #             prices[act] = all_data['currentPrice']
+    #         except:
+    #             prices[act] = all_data['previousClose']
+    # user_with_prices = {}
+    # for usuario in usuarios_interessados:
+    #     user_with_prices[usuario] = {}
+    #     for act in prices:
+    #         if act in usuarios_interessados[usuario]:
+    #             user_with_prices[usuario][act] = prices[act]
                 
-        string_message = create_string(user_with_prices[usuario])
-        sleep(5)
-        await client.send_message(usuario, string_message)
+    #     string_message = create_string(user_with_prices[usuario])
+    #     sleep(3)
+    #     print(usuario)
+    #     print(string_message)
+    #     await client.send_message(usuario, string_message)
